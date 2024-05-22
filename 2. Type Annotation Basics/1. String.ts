188a3e0510c81dcef91433e2aa0def1c003846bc
Kirtigourab Gouda <98795248+kgourab73@users.noreply.github.com>
2024-05-22 19:07:20 +0530

    Add files via upload

---
+++ 2. Type Annotation Basics/1. String.ts
@@ -0,0 +1,8 @@
+// let myVar: type = value
+
+let myName: string = "kirti WebDev";
+myName = "Another Name";
+console.log(myName);
+
+// ERROR
+// myName = 12;
---
+++ 5. Object Types/3. Optional-Properties.ts
@@ -0,0 +1,13 @@
+type User = {
+  name: string;
+  age?: number;
+  location: string;
+};
+
+const user: User = {
+  name: "kirti",
+  age: 20,
+  location: "Arabic",
+};
+
+console.log(`Name: ${user.name}, Age: ${user.age}, Location: ${user.location}`);
